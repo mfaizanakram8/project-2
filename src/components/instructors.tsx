ef89562a6759fce699c2f901676190e585186c36
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-09-21 23:43:29 -0700

    update6

--- src/components/instructors.tsx
+++ src/components/instructors.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from 'react';
-import { WavyBackground } from './ui/wavy-background';
 import faiImage from '@/components/images/fai.png';
 import aliImage from '@/components/images/ali.jpg';
 import asifImage from '@/components/images/asif.jpg';
@@ -30,26 +29,16 @@ const Instructors = [
 
 function Instructor() {
   return (
-    <div className="relative h-[40rem] overflow-hidden flex items-center justify-center">
-      <WavyBackground
-        className='w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full'
-        backgroundFill="#000000"
-        blur={10}
-        speed="medium"
-        waveOpacity={0.5}
-        colors={["#87CEEB", "#FFB6C1", "#3CB371"]} 
-        waveWidth={75}
-      >
-        <h2 className='text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8'>
-          Meet Our Instructors
-        </h2>
-        <p className='text-base md:text-lg text-center mb-4 text-white'>
-          Discover the talented professionals who will guide your coding journey
-        </p>
-        <div className='flex flex-row items-center justify-center mb-10 w-full'>
-          <AnimatedTooltip items={Instructors} />
-        </div>
-      </WavyBackground>   
+    <div className="relative h-[40rem] overflow-hidden flex flex-col items-center justify-center">
+      <h2 className='text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8'>
+        Meet Our Instructors
+      </h2>
+      <p className='text-base md:text-lg text-center mb-4 text-white'>
+        Discover the talented professionals who will guide your coding journey
+      </p>
+      <div className='flex flex-row items-center justify-center mb-10 w-full'>
+        <AnimatedTooltip items={Instructors} />
+      </div>
     </div>
   );
 }
