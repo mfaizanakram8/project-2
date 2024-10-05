72406b638c57658e5c0ce8a597230e8bc5d4b575
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-10-05 09:33:48 -0700

    update8

---
+++ src/components/Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Print a message to the console
+    cout << "Hello, welcome to the sample C++ project!" << endl;
+
+    // Return success exit code
+    return EXIT_SUCCESS;
+}
